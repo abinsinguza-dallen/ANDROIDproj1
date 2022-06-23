6c9692a94ffee5466cbbd7d93ea669fa79a4de2a
-abinsinguza-dallen <dallenabinsinguza@gmail.com>
2022-06-23 13:46:13 +0300

    first commit

--- app/src/main/java/com/myandroidapp/helloworld/BMICalculatorActivity.kt
+++ app/src/main/java/com/myandroidapp/helloworld/BMICalculatorActivity.kt
@@ -4,16 +4,54 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class BMICalculatorActivity : AppCompatActivity() {
-    lateinit var btnSendMoney:Button
+    lateinit var btnCalculate:Button
+    lateinit var etWeight:EditText
+    lateinit var etHeight:EditText
+    lateinit var tvBmi:TextView
+    lateinit var tvanswer:TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_bmicalculator)
-        btnSendMoney=findViewById(R.id.btnSendMoney)
-        btnSendMoney.setOnClickListener {
-            val intent=Intent(this,MobilePesaActivity::class.java)
-            startActivity(intent)
+
+        btnCalculate=findViewById(R.id.btnCalculate)
+        etWeight=findViewById(R.id.etWeight)
+        etHeight=findViewById(R.id.etHeight)
+        tvBmi=findViewById(R.id.tvBmi)
+        tvanswer=findViewById(R.id.tvanswer)
+
+        btnCalculate.setOnClickListener {
+            var weight=etWeight.text.toString()
+            var height=etHeight.text.toString()
+            if (weight. isBlank()){
+                etWeight.error="weight is required"
+                return@setOnClickListener
+            }
+            if (height.isBlank()){
+                etHeight.error ="height is required"
+                return@setOnClickListener
+            }
+            calculateBmi(weight.toDouble(),height.toInt())
+         }
+
+    }
+    fun calculateBmi(weight:Double,height:Int){
+        var heightMetres=height/100.0
+        var bmi=weight/(heightMetres*heightMetres)
+        if (bmi<18.5){
+            tvanswer.text="underweight"
         }
+        if (bmi>18.5 && bmi<25.0 ){
+            tvanswer.text="Normal"
+        }
+        if (bmi>25.0 && bmi<30.0){
+            tvanswer.text="overweight"
+        }
+        if (bmi>30.0)
+            tvanswer.text="obese"
+        tvBmi.text=bmi.toString()
     }
 }
--- app/src/main/java/com/myandroidapp/helloworld/MainActivity.kt
+++ app/src/main/java/com/myandroidapp/helloworld/MainActivity.kt
@@ -7,14 +7,19 @@ import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     lateinit var btnBmi : Button
+    lateinit var btnSendMoney:Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        btnBmi =findViewById(R.id.btnBmi)
+        btnBmi =findViewById(R.id.btnCalculatorBmi)
         btnBmi.setOnClickListener {
           val intent = Intent(this,BMICalculatorActivity::class.java)
             startActivity(intent)
-
+        }
+        btnSendMoney=findViewById(R.id.btnSendMoney)
+        btnSendMoney.setOnClickListener {
+            val intent=Intent(this,MobilePesaActivity::class.java)
+            startActivity(intent)
         }
     }
 }
